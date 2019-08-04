75d0e534e5b88ceea2848faef01298dc8e4bb716
jungahshin <shinasong@naver.com>
2019-08-04 23:59:09 +0900

    11724-3 문제 풀이

---
+++ c:c++/11724-3.cpp
@@ -0,0 +1,54 @@
+//이 문제는 인접 리스트가 훨씬 쉽다...
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+vector<int> input[1001];
+int visited[1001]={0, };
+int connected_component = 0;
+int a,b,m,n ;
+int sum = 0;
+int next = 0;
+
+void dfs(int y){
+    for(int i=0; i<input[y].size(); i++){
+        int next = input[y][i];
+        if(!visited[next]){
+            visited[next] = 1;
+            dfs(next);
+        }
+    }
+}
+
+int main(){
+    //scanf("%d %d", &a, &b);//a-정점, b-간선
+    cin>>a>>b;
+    
+    if(a<1 || a>1000 || b<0 || b>a*(a-1)/2){
+        return 0;
+    }
+
+    for(int i=1; i<=b; i++){//간선의 개수 b만큼 입력을 받는다
+    //scanf("%d %d", &m, &n);//연결된 두 정점 m과 n
+        cin>>m>>n;
+
+        if(m<1 || n>a){
+            return 0;
+        }
+        
+        input[m].push_back(n);//무방향 그래프
+        input[n].push_back(m);
+    }   
+
+    for(int i=1; i<= a; i++){
+        if(!visited[i]){
+            visited[i] = 1;
+            dfs(i);
+            connected_component++;
+        }
+    }
+
+    cout<<connected_component<<"\n";
+}
+
