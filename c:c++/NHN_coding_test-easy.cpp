967cbec3f90b38e62d5b06e8906131734913f09f
jungahshin <shinasong@naver.com>
2019-08-26 02:44:33 +0900

    NHN coding test(쉬운 방법-완료)

--- c:c++/NHN_coding_test-easy.cpp
+++ c:c++/NHN_coding_test-easy.cpp
@@ -15,25 +15,25 @@ int area[101];//구역 번호 저장
 int x_[101];//x좌표 저장
 int y_[101];//y좌표 저장
 int r_[101];//반지름 저장
-int num = 0;
+int num;
+int duplicate = 0;
 bool flag = 0;
 
 int main(){
     cin>>num;//구역 수
 	
 	for(int i =0; i<num; i++){
-		cin>>t;//구역 번호
-		cin>>a;//x좌표
-		cin>>b;//y좌표
-		cin>>r;//반지름 크기
+		cin>>t>>a>>b>>r;//구역 번호
+		// cin>>a;//x좌표
+		// cin>>b;//y좌표
+		// cin>>r;//반지름 크기
 		area[i] = t;
         x_[i] = a;
         y_[i] = b;
         r_[i] = r;
 	}
 
-    cin>>start;//시작 구역 번호
-    cin>>goal;//지도가 있는 구역 번호
+    cin>>start>>goal;//지도가 있는 구역 번호
 
     if(start == goal){
         cout<<start<<"\n";
@@ -80,12 +80,13 @@ int main(){
         for(int i=0; i<start_include.size(); i++){
             for(int j=0; j<goal_include.size(); j++){
                 if(start_include[i].first == goal_include[j].first){//구역이 같으면
-                    num++;//중복의 개수를 센다.
+                    duplicate++;//중복의 개수를 센다.
                 }
             }
         }
 
-        if(num == 0){//하나도 중복 값이 없으면 둘은 전혀 어디에도 포함되지 않는 다른 곳에 있으므로 0을 지나야 한다.
+
+        if(duplicate == 0){//하나도 중복 값이 없으면 둘은 전혀 어디에도 포함되지 않는 다른 곳에 있으므로 0을 지나야 한다.
             reverse(goal_include.begin(), goal_include.end());
             for(int i=0; i<start_include.size(); i++){
                 final.push_back(start_include[i].first);//구역만 넣는다.
@@ -95,7 +96,7 @@ int main(){
                 final.push_back(goal_include[i].first);
             }
 
-        }else if(num == 1){//중복 값이 하나라도 있으면 두 구역은 어느 구역에 같이 포함되어 있는 것
+        }else if(duplicate == 1){//중복 값이 하나라도 있으면 두 구역은 어느 구역에 같이 포함되어 있는 것
             //일단 goal_include함수를 reverse로 순서를 바꾼 후에, start_include에 push_back으로 모두 추가한다.
             reverse(goal_include.begin(), goal_include.end());
             for(int i=0; i<goal_include.size(); i++){
@@ -107,7 +108,7 @@ int main(){
             //그리고 중복검사를 해서 중복이 있으면 하나를 삭제한다.
             final.erase(unique(final.begin(),final.end()),final.end());
 
-        }else if(num >= 2){
+        }else if(duplicate >= 2){
             for(int i=0; i<start_include.size(); i++){
                 for(int j=0; j<goal_include.size(); j++){
                     if(start_include[i].first == goal_include[j].first){
@@ -136,6 +137,7 @@ int main(){
         for(int i=0; i<final.size(); i++){
             cout<<final[i]<<" ";
         }
+        cout<<"\n";
         return 0;
     }
     
