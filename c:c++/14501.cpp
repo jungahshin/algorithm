fcc1e135b0c6343d8471d6268aaa116cfaf474a9
jungahshin <shinasong@naver.com>
2019-10-17 04:21:00 +0900

    14501번 문제 풀이(dfs, dp)

--- c:c++/14501.cpp
+++ c:c++/14501.cpp
@@ -7,19 +7,11 @@ vector<pair<int, int>> v;
 int n, t, p;
 int final_max = 0;
 
-bool check(int idx){
-    bool temp = false;
-    if(v[idx].first == 1 && idx == n-1){
-        temp = true;
-    }
-    return temp;
-}
-
 void dfs(int idx, int final){
     if((idx+1)+(v[idx].first) <= n+1){
         final += v[idx].second;
     }
-    if((idx+1)+(v[idx].first) >= n+1 || check(idx)){
+    if((idx+1)+(v[idx].first) >= n+1){
         //최대값 저장하기
         final_max = max(final_max, final);
         return;
