65a3b1d2956aab043fcede44828e77c19112dfc5
jungahshin <shinasong@naver.com>
2019-11-27 19:53:04 +0900

    1463번 문제 풀이(DP)

--- c:c++/1463.cpp
+++ c:c++/1463.cpp
@@ -7,9 +7,7 @@ int n;
 int num[1000001];//특정 숫자가 1이 되는 데에 최소 연산 횟수 저장
 
 int go(int x){
-    cout<<"x"<<x<<"\n";
     if(x == 1){
-        cout<<"도달"<<"\n";
         return 0;
     }
     if(num[x] != INT_MAX){
@@ -17,18 +15,12 @@ int go(int x){
     }else{
         for(int i=0; i<3; i++){
             if((x % 3) == 0){
-                cout<<"3 in"<<"\n";
                 num[x] = min(num[x], go(x/3)+1);
-                cout<<"num3"<<num[x]<<"\n";
             }
             if((x % 2) == 0){//2번 연산
-                cout<<"2 in"<<"\n";
                 num[x] = min(num[x], go(x/2)+1);
-                cout<<"num2"<<num[x]<<"\n";
             }
-            cout<<"1 in"<<"\n";
             num[x] = min(num[x], go(x-1)+1);
-            cout<<"num1"<<num[x]<<"\n";
         }
     }
     return num[x];
@@ -41,5 +33,4 @@ int main(){
     }
     int temp = go(n);
     cout<<temp<<"\n";
-    // cout<<num[2]<<"\n";
 }
