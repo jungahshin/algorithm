69b2551cb4085b291d59f31d4d9fdb7d5e01c533
jungahshin <shinasong@naver.com>
2019-11-27 18:54:50 +0900

    1937번 문제 풀이(bfs->시간초과,,)

--- c:c++/1937.cpp
+++ c:c++/1937.cpp
@@ -10,6 +10,7 @@ int dy[4] = {-1, 1, 0, 0};
 int final = 0;
 int n;
 int visited[510][510] = {0, };//x, y->도착, z->num
+int way_num[501][501] = {0, };
 
 void go(){
     for(int i=0; i<n; i++){
@@ -24,11 +25,22 @@ void go(){
             }
 
             while(!q.empty()){
+                int temp = false;
                 int x = q.front().first.first;
                 int y = q.front().first.second;
                 int num = q.front().second;
                 q.pop();
                 visited[x][y] = 1;
+
+                if(way_num[x][y] != 0){
+                    cout<<"i"<<i<<"j"<<j<<"\n";
+                    cout<<"x"<<x<<"y"<<y<<"\n";
+                    cout<<"way_num"<<way_num[x][y]<<"\n";
+                    cout<<"총 이동 수"<<num+way_num[x][y]-1<<"\n";
+                    way_num[i][j] = num+way_num[x][y]-1;
+                    final = max(final, num+way_num[x][y]-1);
+                    continue;
+                }
                 //num이 가장 큰 값을 출력한다.
                 final = max(final, num);
             
@@ -37,10 +49,19 @@ void go(){
                     int final_y = y + dy[t];
                     if(0 <= final_x && final_x <n && 0 <= final_y && final_y < n && !visited[final_x][final_y]){
                         if(forest[x][y] < forest[final_x][final_y]){
+                            temp = true;
                             visited[final_x][final_y] = 1;
                             q.push(make_pair(make_pair(final_x, final_y), num+1));              
                         }
                     }
+                }
+                if(temp == false){//더 이상 갈 곳이 없을 경우
+                    cout<<"갈 곳이 없어"<<"\n";
+                    cout<<"i"<<i<<"j"<<j<<"\n";
+                    cout<<"x"<<x<<"y"<<y<<"\n";
+                    cout<<"final"<<final<<"\n";
+                    // way_num[x][y] = num;
+                    way_num[i][j] = num;
                 }
             }
         }
@@ -55,5 +76,12 @@ int main(){
         }
     }
     go();
+    cout<<"way_num 최종"<<"\n";
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout<<way_num[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
     cout<<final<<"\n";
 }
