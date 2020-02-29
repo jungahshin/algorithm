4ada3c3591209e7090c8c7b1fb5b3d8bbcdf4bc3
jungahshin <shinasong@naver.com>
2020-02-29 22:25:27 +0900

    14889번 문제 풀이(복습, 조합)

--- c:c++/14889_review.cpp
+++ c:c++/14889_review.cpp
@@ -9,32 +9,30 @@ int n;
 int a[21][21] = {0, };
 int visited[21] = {0, };
 int visited_[21] = {0, };
+int visited_1[21] = {0, };
 int final = 0;
 int final_ = 0;
 vector<int> v;
+vector<int> v_;
 vector<int> v_final;
+vector<int> v_final_;
 int real_final = INT_MAX;
 //인원수는 무조건 n/2로 나눠야 한다.
 
-void cal_2(int check){
+void cal_2(){
     v_final.clear();
     for(int i=0; i<n; i++){
         if(visited_[i] == true){
             v_final.push_back(i);
         }
     }
-    if(check == 0){
-        final += a[(v_final[0])][(v_final[1])];
-        final += a[(v_final[1])][(v_final[0])];
-    }else{
-        final_ += a[(v_final[0])][(v_final[1])];
-        final_ += a[(v_final[1])][(v_final[0])];        
-    }
+    final += a[(v_final[0])][(v_final[1])];
+    final += a[(v_final[1])][(v_final[0])];
 }
 
-void go_2(int num, int idx, int check){//n/2중에서 2개를 뽑는다.
+void go_2(int num, int idx){//n/2중에서 2개를 뽑는다.
     if(num == 2){
-        cal_2(check);
+        cal_2();
         return;
     }
 
@@ -42,30 +40,53 @@ void go_2(int num, int idx, int check){//n/2중에서 2개를 뽑는다.
         if(visited_[v[i]] == true) continue;
         visited_[v[i]] = true;
 
-        go_2(num+1, i, check);
+        go_2(num+1, i);
         visited_[v[i]] = false;
+    }
+}
+
+void cal_3(){
+    v_final_.clear();
+    for(int i=0; i<n; i++){
+        if(visited_1[i] == true){
+            v_final_.push_back(i);
+        }
+    }
+    final_ += a[(v_final_[0])][(v_final_[1])];
+    final_ += a[(v_final_[1])][(v_final_[0])];
+}
+
+void go_3(int num, int idx){
+    if(num == 2){
+        cal_3();
+        return;
+    }
+
+    for(int i=idx; i<v_.size(); i++){
+        if(visited_1[v_[i]] == true) continue;
+        visited_1[v_[i]] = true;
+
+        go_3(num+1, i);
+        visited_1[v_[i]] = false;
     }
 }
 
 void cal(){
     v.clear();
+    v_.clear();
     for(int i=0; i<n; i++){
         if(visited[i] == true){//스타트팀
             v.push_back(i);
+        }else{
+            v_.push_back(i);
         }
     }
+    memset(visited_1, 0, sizeof(visited_1));
+    final_ = 0;
+    go_3(0, 0);
     memset(visited_, 0, sizeof(visited_));
     final = 0;
-    go_2(0, 0, 0);
-    v.clear();
-    for(int i=0; i<n; i++){
-        if(visited[i] == false){//스타트팀
-            v.push_back(i);
-        }
-    }
-    memset(visited_, 0, sizeof(visited_));
-    final_ = 0;
-    go_2(0, 0, 1);
+    go_2(0, 0);
     real_final = min(abs(final-final_), real_final);
 }
 
