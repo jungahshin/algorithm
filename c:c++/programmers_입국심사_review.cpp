135b8ea42822af2f395e982eab558bed9527cda8
jungahshin <shinasong@naver.com>
2021-01-10 23:20:16 +0900

    [Programmers] 입국심사

---
+++ c:c++/programmers_입국심사_review.cpp
@@ -0,0 +1,34 @@
+l#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <climits>
+ 
+using namespace std;
+ 
+long long solution(int n, vector<int> times) {
+ 
+    sort(times.begin(), times.end());
+ 
+    long long Start = 1;
+    long long End = (long long)times[times.size()-1]*n;
+    long long answer = End;
+    
+    while(Start<=End){
+        long long mid = (Start+End)/2;
+        long long sum = 0;
+        for(int i=0; i<times.size(); i++){
+            sum += (mid/times[i]);
+        }
+                
+        if(sum>=n){
+            answer = min(answer, mid);
+            End = mid-1;
+        }else if(sum<n){
+            Start = mid+1;
+        }
+        
+    }
+    
+    return answer;
+}
